1abab7c57874462e984fefb944f7e1e5d315c833
Philip Noebauer <pnoebauer>
2021-09-12 10:41:10 +0800

    functionality added for deleting comments based on id and email

--- test/delete-comments.test.js
+++ test/delete-comments.test.js
@@ -1,66 +1,66 @@
-import CommentsDAO from "../src/dao/commentsDAO"
-import MoviesDAO from "../src/dao/moviesDAO"
+import CommentsDAO from '../src/dao/commentsDAO';
+import MoviesDAO from '../src/dao/moviesDAO';
 
 const testUser = {
-  name: "foobar",
-  email: "foobar@baz.com",
-}
+	name: 'foobar',
+	email: 'foobar@baz.com',
+};
 
 const newUser = {
-  name: "barfoo",
-  email: "baz@foobar.com",
-}
+	name: 'barfoo',
+	email: 'baz@foobar.com',
+};
 
 // this is the movieId for "King Kong"
-const movieId = "573a1392f29313caabcd9f92"
+const movieId = '573a1392f29313caabcd9f92';
 
-const date = new Date()
+const date = new Date();
 
 let comment = {
-  text: "fe-fi-fo-fum",
-  id: "",
-}
+	text: 'fe-fi-fo-fum',
+	id: '',
+};
 
-describe("Delete Comments", () => {
-  beforeAll(async () => {
-    await CommentsDAO.injectDB(global.mflixClient)
-    await MoviesDAO.injectDB(global.mflixClient)
-  })
+describe('Delete Comments', () => {
+	beforeAll(async () => {
+		await CommentsDAO.injectDB(global.mflixClient);
+		await MoviesDAO.injectDB(global.mflixClient);
+	});
 
-  test("Can post a comment", async () => {
-    const postCommentResult = await CommentsDAO.addComment(
-      movieId,
-      testUser,
-      comment.text,
-      date,
-    )
+	test('Can post a comment', async () => {
+		const postCommentResult = await CommentsDAO.addComment(
+			movieId,
+			testUser,
+			comment.text,
+			date,
+		);
 
-    expect(postCommentResult.insertedCount).toBe(1)
-    expect(postCommentResult.insertedId).not.toBe(null)
+		expect(postCommentResult.insertedCount).toBe(1);
+		expect(postCommentResult.insertedId).not.toBe(null);
 
-    const kingKongComments = (await MoviesDAO.getMovieByID(movieId)).comments
+		const kingKongComments = (await MoviesDAO.getMovieByID(movieId)).comments;
 
-    expect(kingKongComments[0]._id).toEqual(postCommentResult.insertedId)
-    expect(kingKongComments[0].text).toEqual(comment.text)
+		expect(kingKongComments[0]._id).toEqual(postCommentResult.insertedId);
+		expect(kingKongComments[0].text).toEqual(comment.text);
 
-    comment.id = postCommentResult.insertedId
-  })
+		comment.id = postCommentResult.insertedId;
+	});
 
-  test("Cannot delete a comment if email does not match", async () => {
-    const deleteCommentResult = await CommentsDAO.deleteComment(
-      comment.id,
-      newUser.email,
-    )
+	test('Cannot delete a comment if email does not match', async () => {
+		const deleteCommentResult = await CommentsDAO.deleteComment(
+			comment.id,
+			newUser.email,
+		);
 
-    expect(deleteCommentResult.deletedCount).toBe(0)
-  })
+		expect(deleteCommentResult.deletedCount).toBe(0);
+	});
 
-  test("Can delete a comment if email matches", async () => {
-    const deleteCommentResult = await CommentsDAO.deleteComment(
-      comment.id,
-      testUser.email,
-    )
+	test('Can delete a comment if email matches', async () => {
+		const deleteCommentResult = await CommentsDAO.deleteComment(
+			comment.id,
+			testUser.email,
+		);
 
-    expect(deleteCommentResult.deletedCount).toBe(1)
-  })
-})
+		expect(deleteCommentResult.deletedCount).toBe(1);
+	});
+});
